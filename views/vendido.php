e2c4bf0a457f1a5ad81259501380589dd43122fb
MarcoPSM <marcopaulomartins@hotmail.com>
2021-12-22 00:04:56 +0000

    Slide with controls

--- views/vendido.php
+++ views/vendido.php
@@ -10,21 +10,26 @@ $this->activeMenu = 'vendido';
 <br>
 <div id="vendido-content">
 
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <!-- <img src="https://triomphe.pt/website/image/product.product/32269_7faa5bd/web_image" class="d-block w-100" alt="..."> -->
-                <img src="img/vendidas/LoftVendaNova900x600.jpg" class="d-block w-100" alt="LoftVendaNova900x600" >
+                <img src="img/vendidas/LoftVendaNova900x600.jpg" class="d-block w-100" alt="LoftVendaNova900x600">
             </div>
             <div class="carousel-item">
-                <!-- <img src="https://triomphe.pt/website/image/product.product/25397_c11a322/web_image" class="d-block w-100" alt="..."> -->
-                <img src="img/vendidas/MoradiaAzenhasDoMar900x600.jpg" class="d-block w-100" alt="MoradiaAzenhasDoMar900x600" >
+                <img src="img/vendidas/MoradiaAzenhasDoMar900x600.jpg" class="d-block w-100" alt="MoradiaAzenhasDoMar900x600">
             </div>
             <div class="carousel-item">
-                <!-- <img src="https://triomphe.pt/website/image/product.product/21040_c652dca/web_image" class="d-block w-100" alt="..." height="600px"> -->
-                <img src="img/vendidas/MoradiaSantaCruz-TorresVedras900x617.jpg" class="d-block w-100" alt="MoradiaSantaCruz-TorresVedras900x617" >
+                <img src="img/vendidas/MoradiaSantaCruz-TorresVedras900x617.jpg" class="d-block w-100" alt="MoradiaSantaCruz-TorresVedras900x617">
             </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 
 
